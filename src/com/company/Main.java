cd46992574151fcb45d8f14b3c7b13bda9d48961
akylaitologonova <tologonova.lr@gmail.com>
2021-06-09 23:29:18 +0600

    qa

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,20 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
 	// write your code here
-        Hero Superman = new Hero(250, 50, 80);
+        Hero superman = new Hero(250, 50, 80);
 
 
-        System.out.println(Superman.getHealth() + " " +
-                Superman.getDamage() + " " + Superman.getSuperAbilities());
+        System.out.println(superman.getHealth() + " " +
+                superman.getDamage() + " " + superman.getSuperAbilities());
 
 
-        Boss Ultraman  = new Boss(400, 80, 0);
+        Boss ultraman  = new Boss(400, 80, 0);
 
 
-
-
-        System.out.println(Ultraman.getHealth() + " " +
-                Ultraman.getDamage() + " " + Ultraman.getProtect());
+        System.out.println(ultraman.getHealth() + " " +
+                ultraman.getDamage() + " " + ultraman.getProtect());
 
 
     }
